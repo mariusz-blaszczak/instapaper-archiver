ecb94b567b9c5fc5cea5de3794cd7b7f04f5b8d3
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-09-12 13:17:22 +0200

    add empty task

---
+++ lib/tasks/instapaper.rake
@@ -0,0 +1,5 @@
+namespace :instapaper do
+  desc "Archives all current articles on users profile of instapaper"
+  task archive: :environment do
+  end
+end
--- spec/tasks/archive_spec.rb
+++ spec/tasks/archive_spec.rb
@@ -2,6 +2,6 @@ require "rails_helper"
 
 describe "rake instapaper:archive", type: :task do
   it "archives all articles on instapaper account" do
-    
+    task.execute
   end
 end
