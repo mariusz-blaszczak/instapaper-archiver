20cc970db5c37314fdb5c2a7a1ca5ca44e3153f4
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-09-12 14:51:30 +0200

    refactor

--- app/use_cases/instapaper/archive.rb
+++ app/use_cases/instapaper/archive.rb
@@ -2,19 +2,33 @@ module Instapaper
   class Archive
     def call
       login
+      archive_all
     end
 
     def login
+      username = Rails.application.secrets.fetch(:instapaper_login)
+      password = Rails.application.secrets.fetch(:instapaper_password)
+
       browser.goto("https://www.instapaper.com/")
       browser.link(text: "Sign In").click
-      browser.text_field(id: "sign_in_modal_email").set(Rails.application.secrets.fetch(:instapaper_login))
-      browser.text_field(id: "password").set(Rails.application.secrets.fetch(:instapaper_password))
+      browser.text_field(id: "sign_in_modal_email").set(username)
+      browser.text_field(id: "password").set(password)
       browser.button(text: "Sign In").click
+    end
 
+    def archive_all
       while is_there_more? do
-        browser.link(class: %w(js_archive_single action_link)).click
-        sleep(2)
+        archive_first_article_from_the_top
+        wait_for_animation_of_deleting
       end
+    end
+
+    def wait_for_animation_of_deleting
+        sleep(2)
+    end
+
+    def archive_first_article_from_the_top
+      browser.link(class: %w(js_archive_single action_link)).click
     end
 
     def is_there_more?
