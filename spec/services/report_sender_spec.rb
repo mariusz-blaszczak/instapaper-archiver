83825bd1286fe8ee0ba176b7675c01bf74c695df
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-09-13 10:46:33 +0200

    add report sender class

--- spec/services/report_sender_spec.rb
+++ spec/services/report_sender_spec.rb
@@ -1,7 +1,10 @@
 require "rails_helper"
 
-describe "ReportSender" do
-  it "sends SMS report to specified number" do
-    
+describe ReportSender do
+  let(:sender) { instance_double(SmsSender) }
+  subject { described_class.new(sender: sender, sender_name: "Admin", receiver_name: "User", message: "Hello User") }
+  it "cals sender with " do
+    expect(sender).to receive(:send).with(sender: "Admin", receiver: "User", message: "Hello User")
+    subject.send()
   end
 end
