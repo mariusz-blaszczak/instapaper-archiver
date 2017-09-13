cbe3c591362a01218a3dc6f60b2b902292cdbba5
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-09-13 12:58:56 +0200

    extract browser to module

--- app/use_cases/instapaper/archive.rb
+++ app/use_cases/instapaper/archive.rb
@@ -1,5 +1,7 @@
 module Instapaper
   class Archive
+    include Browser
+
     def call
       @archived_count = 0
       login
@@ -41,40 +43,5 @@ module Instapaper
     def is_there_more?
       browser.link(class: %w(js_archive_single action_link)).exists?
     end
-    
-    def browser
-      @browser ||= new_browser
-    end
-
-    def new_browser
-      options = Selenium::WebDriver::Chrome::Options.new
-
-      # make a directory for chrome if it doesn't already exist
-      chrome_dir = File.join Dir.pwd, %w(tmp chrome)
-      FileUtils.mkdir_p chrome_dir
-      user_data_dir = "--user-data-dir=#{chrome_dir}"
-      # add the option for user-data-dir
-      options.add_argument user_data_dir
-
-      # let Selenium know where to look for chrome if we have a hint from
-      # heroku. chromedriver-helper & chrome seem to work out of the box on osx,
-      # but not on heroku.
-      if chrome_bin = ENV["GOOGLE_CHROME_BIN"]
-        options.add_argument "no-sandbox"
-        options.binary = chrome_bin
-        # give a hint to here too
-        Selenium::WebDriver::Chrome.driver_path = \
-          "/app/vendor/bundle/bin/chromedriver"
-      end
-
-      # headless!
-      # keyboard entry wont work until chromedriver 2.31 is released
-      options.add_argument "window-size=1200x600"
-      options.add_argument "headless"
-      options.add_argument "disable-gpu"
-
-      # make the browser
-      Watir::Browser.new :chrome, options: options
-      end
   end
 end
