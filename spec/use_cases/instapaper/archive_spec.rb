f97d1cfdfd5202a4a10f58f0bf9d4e1318b67f95
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-09-14 12:20:02 +0200

    add not saturday context

--- spec/use_cases/instapaper/archive_spec.rb
+++ spec/use_cases/instapaper/archive_spec.rb
@@ -25,4 +25,24 @@ describe Instapaper::Archive do
       subject
     end
   end
+
+  context "on other day than saturyday" do
+    let(:sample_sunday) { Time.new(2017, 9, 3) }
+    before { Timecop.freeze(sample_sunday) }
+    after { Timecop.return }
+
+    let(:archiver) do
+      archiver = instance_double(InstapaperCrawler)
+      allow(archiver).to receive(:archive_all_articles) { Resonad::Success("A sample message") }
+      archiver
+    end
+
+    subject { described_class.new(notifier: notifier, archiver: archiver).call }
+
+
+    it "does not call archive_all_articles on archiver" do
+      expect(archiver).to_not receive(:archive_all_articles)
+      subject
+    end
+  end
 end
