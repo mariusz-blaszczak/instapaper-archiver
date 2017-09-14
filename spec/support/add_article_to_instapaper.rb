12865cc4f58be7737ba9e122caa44ab6bd4d39a3
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-09-14 14:27:51 +0200

    install rest client gem

---
+++ spec/support/add_article_to_instapaper.rb
@@ -0,0 +1,15 @@
+module InstapaperTestSupport
+  class ArticleAdder
+    def initialize
+    end
+
+    def call
+      url = "https://www.instapaper.com/api/add"
+    end
+  end
+end
+
+
+describe InstapaperTestSupport do
+
+end
