8ad9f721220c0e5825b569ca1c5ad0e60fd6bf53
Mariusz Błaszczak <mariusz.blaszczak@gmail.com>
2017-09-14 15:04:13 +0200

    rm binding pry

--- spec/services/instapaper_crawler_spec.rb
+++ spec/services/instapaper_crawler_spec.rb
@@ -6,7 +6,6 @@ describe InstapaperCrawler do
     InstapaperTestSupport::ArticleAdder.new.call("https://roweroweporady.pl/rowerowa-trasa-przemysl-bieszczady-lublin/")
     InstapaperTestSupport::ArticleAdder.new.call("https://roweroweporady.pl/skoda-ebike-test-roweru-elektrycznego/")
     articles_count = InstapaperTestSupport::ArticleCounter.new.call
-    binding.pry
     expect(articles_count).to eq(2)
     subject.call
     articles_count_after_archive = InstapaperTestSupport::ArticleCounter.new.call
